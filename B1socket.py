c0eed51caffcf4940258c5718dfc4c597e186a51
chi <s225255856@deakin.edu.au>
2026-08-13 11:10:27 +1000

    Create B1socket.py

---
+++ B1socket.py
@@ -0,0 +1,22 @@
+import socket
+
+server = '192.168.1.112'
+port = 110
+username = 'ustest'
+password = 'test'
+
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((server, port))
+    response = s.recv(1024).decode()
+    s.sendall(f'USER {username}\r\n'.encode())
+    response = s.recv(1024).decode()
+    s.sendall(f'PASS {password}\r\n'.encode())
+    response = s.recv(1024).decode()
+    s.close()
+    print("\n Done!")
+
+except socket.error as e:
+    print(f"Socket error {e}")
+except Exception as e:
+    print(f"Unexpected error: {e}")
